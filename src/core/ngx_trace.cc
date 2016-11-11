18efb2aa68d56d1733a1f64bae76de69a594f8fa
sensssz <hjmsens@gmail.com>
2016-11-11 14:46:47 -0500

    Set path count to 7.

--- src/core/ngx_trace.cc
+++ src/core/ngx_trace.cc
@@ -17,7 +17,7 @@ using std::string;
 using std::to_string;
 using std::set;
 
-#define TARGET_PATH_COUNT 6
+#define TARGET_PATH_COUNT 7
 #define NUMBER_OF_FUNCTIONS 2
 #define LATENCY
 #define MONITOR
