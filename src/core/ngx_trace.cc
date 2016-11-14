94fb03f1e8fb73816092d05c4b377e8bd68c76e9
sensssz <hjmsens@gmail.com>
2016-11-14 12:58:43 -0500

    Trace ngx_http_free_request.

--- src/core/ngx_trace.cc
+++ src/core/ngx_trace.cc
@@ -17,8 +17,8 @@ using std::string;
 using std::to_string;
 using std::set;
 
-#define TARGET_PATH_COUNT 6
-#define NUMBER_OF_FUNCTIONS 5
+#define TARGET_PATH_COUNT 7
+#define NUMBER_OF_FUNCTIONS 10
 #define LATENCY
 #define MONITOR
 
