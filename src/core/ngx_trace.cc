8e2dfa2336f84c36675548241c7cf469a62ff326
sensssz <hjmsens@gmail.com>
2016-11-14 13:16:10 -0500

    Trace ngx_http_close_connection.

--- src/core/ngx_trace.cc
+++ src/core/ngx_trace.cc
@@ -17,8 +17,8 @@ using std::string;
 using std::to_string;
 using std::set;
 
-#define TARGET_PATH_COUNT 9
-#define NUMBER_OF_FUNCTIONS 7
+#define TARGET_PATH_COUNT 7
+#define NUMBER_OF_FUNCTIONS 5
 #define LATENCY
 #define MONITOR
 
